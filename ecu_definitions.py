c26375d8d7f37488b719a03792b1152a7a097293
dmg210 <dmg@dg21network.com>
2024-02-19 17:21:38 +0000

    separate 5WY17 and 5WY18+

--- ecu_definitions.py
+++ ecu_definitions.py
@@ -37,19 +37,37 @@ ECU_IDENTIFICATION_TABLE = [
 			'program_section_flash_size': 0x5FFF0,
 			'program_section_flash_bin_offset': 0x20010,
 			'program_section_flash_memory_offset': 0x10
-		}
+		},
 	},
 	{
 		'offset': 0x88040,
-		'expected': [[99, 97, 54, 53, 52], [99, 97, 54, 53, 53]], #CA654, CA655
+		'expected': [[99, 97, 54, 53, 52, 48, 49]], #CA65401 (5WY17)
 		'ecu': {
-			'name': 'SIMK43 V6 4mbit',
+			'name': 'SIMK43 V6 4mbit (5WY17)',
 			'eeprom_size_bytes': 524288, # (512 KiB)
 			'memory_offset': -0x8000,
 			'bin_offset': -0x88000,
 			'memory_write_offset': -0x7800,
 			'calibration_size_bytes': 0x8000, # 32,768 bytes (32 KiB)
 			'calibration_size_bytes_flash': 0x5F00,
+			'program_section_offset': 0x98000,
+			'program_section_size': 0x70000,
+			'program_section_flash_size': 0x6FFF0,
+			'program_section_flash_bin_offset': 0x10010,
+			'program_section_flash_memory_offset': 0x10
+		}
+	},
+		{
+		'offset': 0x88040,
+		'expected': [[99, 97, 54, 53, 52], [99, 97, 54, 53, 53]], #CA654, CA655 (5WY18+)
+		'ecu': {
+			'name': 'SIMK43 V6 4mbit (5WY18+)',
+			'eeprom_size_bytes': 524288, # (512 KiB)
+			'memory_offset': -0x8000,
+			'bin_offset': -0x88000,
+			'memory_write_offset': -0x7800,
+			'calibration_size_bytes': 0x8000, # 32,768 bytes (32 KiB)
+			'calibration_size_bytes_flash': 0x6F00,
 			'program_section_offset': 0x98000,
 			'program_section_size': 0x70000,
 			'program_section_flash_size': 0x6FFF0,
