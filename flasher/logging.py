bc64f2fe977b4f033e0e2dcf3298f8ac6606c6ff
Piotr Zdolski <dante383@protonmail.com>
2024-04-29 22:05:15 +0200

    add precision on floats

--- flasher/logging.py
+++ flasher/logging.py
@@ -10,158 +10,180 @@ data_id1 = [
 		'position': 38,
 		'size': 2,
 		'conversion': lambda a: a * 4.883,
+		'precision': 1,
 	},
 	{
 		'name': 'Air Flow Rate from Mass Air Flow Sensor',
 		'unit': 'kg/h',
 		'position': 15,
 		'size': 2,
-		'conversion': lambda a: a * 0.03125
+		'conversion': lambda a: a * 0.03125,
+		'precision': 2
 	},
 	{
 		'name': 'Engine Coolant Temperature Sensor',
 		'unit': 'C',
 		'position': 4,
 		'size': 1,
-		'conversion': lambda a: a * 0.75 
+		'conversion': lambda a: a * 0.75,
+		'precision': 2 
 	},
 	{
 		'name': 'Oil Temperature Sensor',
 		'unit': 'C',
 		'position': 6,
 		'size': 1,
-		'conversion': lambda a: (a * 1)-40
+		'conversion': lambda a: (a * 1)-40,
+		'precision': 2
 	},
 	{
 		'name': 'Battery voltage',
 		'position': 1,
 		'size': 1,
 		'unit': 'V',
-		'conversion': lambda a: a * 0.10159
+		'conversion': lambda a: a * 0.10159,
+		'precision': 2
 	},
 	{
 		'name': 'Intake Air Temperature Sensor',
 		'position': 9,
 		'size': 1,
 		'unit': 'C',
-		'conversion': lambda a: (a*0.75)-48
+		'conversion': lambda a: (a*0.75)-48,
+		'precision': 2
 	},
 	{
 		'name': 'Throttle Position',
 		'position': 11,
 		'size': 1,
 		'unit': "'",
-		'conversion': lambda a: a * 0.468627
+		'conversion': lambda a: a * 0.468627,
+		'precision': 2
 	},
 	{
 		'name': 'Vehicle Speed',
 		'position': 30,
 		'size': 1,
 		'unit': 'km/h',
-		'conversion': lambda a : a
+		'conversion': lambda a : a,
+		'precision': 1
 	},
 	{
 		'name': 'Engine Speed',
 		'position': 31,
 		'size': 2,
 		'unit': 'RPM',
-		'conversion': lambda a: a
+		'conversion': lambda a: a,
+		'precision': 1
 	},
 	{
 		'name': 'Oxygen Sensor-Bank1/Sensor2',
 		'position': 40,
 		'size': 2,
 		'unit': 'mV',
-		'conversion': lambda a: a * 4.883
+		'conversion': lambda a: a * 4.883,
+		'precision': 2
 	},
 	{
 		'name': 'Ignition Timing Advance for 1 Cylinder',
 		'position': 58,
 		'size': 1,
 		'unit': "'",
-		'conversion': lambda a: (a*-0.325)-72
+		'conversion': lambda a: (a*-0.325)-72,
+		'precision': 2
 	},
 	{
 		'name': 'Cylinder Injection Time-Bank1',
 		'position': 76,
 		'size': 2,
 		'unit': 'ms',
-		'conversion': lambda a: a * 0.004
+		'conversion': lambda a: a * 0.004,
+		'precision': 2
 	},
 	{
 		'name': 'Long Term Fuel Trim-Idle Load',
 		'position': 89,
 		'size': 2,
 		'unit': 'ms',
-		'conversion': lambda a: a * 0.004
+		'conversion': lambda a: a * 0.004,
+		'precision': 2
 	},
 	{
 		'name': 'Long Term Fuel Trim-Part Load',
 		'position': 91,
 		'size': 2,
 		'unit': '%',
-		'conversion': lambda a: a * 0.001529
+		'conversion': lambda a: a * 0.001529,
+		'precision': 2
 	},
 	{
 		'name': 'Camshaft Actual Position',
 		'position': 142,
 		'size': 1,
 		'unit': "'",
-		'conversion': lambda a: (a*0.375)-60
+		'conversion': lambda a: (a*0.375)-60,
+		'precision': 2
 	},
 	{
 		'name': 'Camshaft position target',
 		'position': 143,
 		'size': 1,
 		'unit': "'",
-		'conversion': lambda a: (a*0.375)-60
+		'conversion': lambda a: (a*0.375)-60,
+		'precision': 2
 	},
 	{
 		'name': 'Ignition dwell time',
 		'position': 106,
 		'size': 2,
 		'unit': 'ms',
-		'conversion': lambda a: a*0.004
+		'conversion': lambda a: a*0.004,
+		'precision': 2
 	},
 	{
 		'name': 'EVAP Purge valve',
 		'position': 101,
 		'size': 2,
 		'unit': '%',
-		'conversion': lambda a: a*0.003052
+		'conversion': lambda a: a*0.003052,
+		'precision': 2
 	},
 	{
 		'name': 'Idle speed control actuator',
 		'position': 99,
 		'size': 2,
 		'unit': '%',
-		'conversion': lambda a: a*0.001529
+		'conversion': lambda a: a*0.001529,
+		'precision': 2
 	},
 	{
 		'name': 'CVVT Valve Duty',
 		'position': 156,
 		'size': 2,
 		'unit': '%',
-		'conversion': lambda a: a * 0.001526
+		'conversion': lambda a: a * 0.001526,
+		'precision': 2
 	},
 	{
 		'name': 'Oxygen Sensor Heater Duty-Bank1/Sensor1',
 		'position': 93,
 		'size': 1,
 		'unit': '%',
-		'conversion': lambda a: a*0.390625
+		'conversion': lambda a: a*0.390625,
+		'precision': 2
 	},
 	{
 		'name': 'Oxygen Sensor Heater Duty-Bank1/Sensor2',
-		'position': 93,
+		'position': 94,
 		'size': 1,
 		'unit': '%',
-		'conversion': lambda a: a*0.390625
+		'conversion': lambda a: a*0.390625, 
+		'precision': 2
 	}
 ]
 
 def grab (payload, parameter):
-	return parameter['conversion'](int.from_bytes(payload[parameter['position']:parameter['position']+parameter['size']], "little"))
+	return round(parameter['conversion'](int.from_bytes(payload[parameter['position']:parameter['position']+parameter['size']], "little")), parameter['precision'])
 
 def poll (ecu):
 	data = [int(time.time())]
@@ -169,7 +191,7 @@ def poll (ecu):
 	for parameter in data_id1:
 		value = grab(raw_data, parameter)
 		data.append(value)
-		print('{}: {} {}'.format(parameter['name'], value, parameter['unit']))
+		print('{}: {}{}'.format(parameter['name'], value, parameter['unit']))
 	return data
 
 def logger(ecu):
