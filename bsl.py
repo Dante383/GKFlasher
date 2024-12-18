5815fcdbb962a8f4973c146ec6f9b64d26c3acdf
dmg210 <dmg@dg21network.com>
2024-12-18 17:05:46 +0000

    absolute path for base

--- bsl.py
+++ bsl.py
@@ -31,7 +31,7 @@ def set_gui_log_handler(gui_callback):
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    base_path = (os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
 def ResetAdapter(ser):
